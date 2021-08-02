1e92f865bc00f84dddc3a0210f30d22d06586c2e
OConnorsphysics <88222724+OConnorsphysics@users.noreply.github.com>
2021-08-01 20:07:25 -0600

    Update Stress_state_from_csv.py
    I have added an import to grab a single data point form the csv file. Ideally this can become a real time frequency of the brain wave. The while loop should run until a keyboard interrupt is made or the loop hits the if i=16 statement(end of file). This statement will have to be changed once we have updating data. Hoping to next add a pyplot with updating display for each iteration of the loop.  The file path will need to be corrected, I just have it set for local use.

--- Stress_state_from_csv.py
+++ Stress_state_from_csv.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sun Aug  1 10:03:05 2021
 
-@authors: Liam O'Connor, Hiranya Maharaja
+@author: Liam O'Connor, Hiryana Maharaja
 """
 import sys
 from math import sqrt, acos, pi, sin
@@ -17,19 +16,29 @@ import pandas as pd
 import sqlite3
 from scipy import fft
 
-filepath = 'SampleCSVFile_2kb.csv'  #File containing EEG data we want to open and monitor
+filepath = 'C:\python\PyQt5\SampleCSVFile_2kb.csv'  #File containing EEG data we want to open and monitor
 STRESSSTATE = 0     #global variable for stress level as percentage
+RUNCONDITION = True #variable to keep loop running based on user input
 
-
-df = pd.read_csv(filepath)
-print(df)
-
-
-
-
-if STRESSSTATE >= 75:
-    print("The child is highly stressed")
-elif (STRESSSTATE < 75 and STRESSSTATE >=50):
-    print ("The child is moderatley stressed out")
-else:
-    print ("The child is not noticeably stressed out")
+df = pd.read_csv(filepath)  #pandas library reads file into a data frame
+i=0         #variable toiterate through lines of csv file
+try:            #try loop to allow keyboard interrupt to terminate program
+    while RUNCONDITION:
+        #print(df)       #prints the data frame 
+        print(i)
+        print(df.iat[i,1]) #df.iat returns the value at given indices
+        STRESSSTATE = df.iat[i,1] #update the stressstate (frequency hopefully) to the new line in file.
+        
+        if STRESSSTATE >= 75:
+            print("The child is highly stressed")
+        elif (STRESSSTATE < 75 and STRESSSTATE >=50):
+            print ("The child is moderatley stressed out")
+        else:
+            print ("The child is not noticeably stressed out")
+                    
+        if i==16:          #set to number of lines in file to end loop without keyboard interrupt
+           break
+        i += 1
+except KeyboardInterrupt:           #key input should pause loop and allow termination
+    print("Press Ctrl-C to terminate the program/loop.")
+    pass
