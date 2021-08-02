79ba0b72e9d75787bb4294684562dda75a5469bf
OConnorsphysics <88222724+OConnorsphysics@users.noreply.github.com>
2021-08-01 23:52:55 -0600

    Update Stress_state_from_csv.py

--- Stress_state_from_csv.py
+++ Stress_state_from_csv.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 """
 Created on Sun Aug  1 10:03:05 2021
 
-@author: Liam O'Connor, Hiryana Maharaja, Jenny Wei
+@author: Liam O'Connor, Hiryana Maharaja, Harleen Kaur, and Jenny Wei
 """
 import sys
 from math import sqrt, acos, pi, sin
@@ -19,6 +20,7 @@ from scipy import fft
 filepath = 'C:\python\PyQt5\SampleCSVFile_2kb.csv'  #File containing EEG data we want to open and monitor
 STRESSSTATE = 0     #global variable for stress level as percentage
 RUNCONDITION = True #variable to keep loop running based on user input
+PlottingList =[]
 
 df = pd.read_csv(filepath)  #pandas library reads file into a data frame
 i=0         #variable toiterate through lines of csv file
@@ -27,15 +29,23 @@ try:            #try loop to allow keyboard interrupt to terminate program
         #print(df)       #prints the data frame 
         print(i)
         print(df.iat[i,1]) #df.iat returns the value at given indices
-        STRESSSTATE = df.iat[i,1] #update the stressstate (frequency hopefully) to the new line in file.
         
-        if STRESSSTATE >= 75:  # Beta waves 18-22 Hz
+        STRESSSTATE = df.iat[i,1] #update the stressstate (frequency hopefully) to the new line in file.
+        PlottingList.append(STRESSSTATE)
+        print("plotting list = " + str(PlottingList[i]))
+        
+        if STRESSSTATE >= 75:
             print("The child is highly stressed")
-        elif (STRESSSTATE < 75 and STRESSSTATE >=50): # Beta waves 22-26 Hz
-            print ("The child is moderately stressed out")
-        else: # Beta waves 26-30 Hz
+        elif (STRESSSTATE < 75 and STRESSSTATE >=50):
+            print ("The child is moderatley stressed out")
+        else:
             print ("The child is not noticeably stressed out")
-                    
+        #plt.plot(i,STRESSSTATE, 'ro') #plots current data of loop only
+        plt.plot(PlottingList)
+        plt.axis([0,50,0,100])
+        plt.xlabel('Time')
+        plt.ylabel('Frequency (Hz)')
+        plt.show()           
         if i==16:          #set to number of lines in file to end loop without keyboard interrupt
            break
         i += 1
